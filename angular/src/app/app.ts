4c8c4363497875a10065869aad5959c6825da777
FilipeNV1 <filipeviseu@ua.pt>
2026-05-31 18:41:52 +0100

    nav not on login

--- angular/src/app/app.ts
+++ angular/src/app/app.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/components/navbar/navbar';
 import { ToastComponent } from './shared/components/toast/toast';
 
@@ -9,4 +9,6 @@ import { ToastComponent } from './shared/components/toast/toast';
   imports: [RouterOutlet, NavbarComponent, ToastComponent],
   templateUrl: './app.html',
 })
-export class App {}
+export class App {
+  constructor(public router: Router) {}
+}
